f01783e170e529c717e15ed2fdda0000fa5ef0e3
Joël Rimaz <jo.ante2011@gmail.com>
2023-12-14 16:32:52 +0100

    show that i consider my chromatic aberration animation as final

--- JS/main.js
+++ JS/main.js
@@ -70,8 +70,8 @@ loadShaderURL("final1", null, "/shaders/macintoshScreen.frag");
 loadShaderURL("final2", null, "/shaders/oldVHS.frag");
 // loadShaderURL("test6", null, "/shaders/noiseTest.frag");
 // loadShaderURL("test7", null, "/shaders/chromaAberration.frag");
-// loadShaderURL("test8", null, "/shaders/chromaAberrationAnim.frag");
+loadShaderURL("final3", null, "/shaders/chromaAberrationAnim.frag");
 
 // usePostEffect("final1", () => ({ "u_time": time(), "u_resy": height()}));
 // usePostEffect("final2", () => ({ "u_time": time() }));
-// usePostEffect("test8", () => ({ "u_time": time(), "u_amount": 4, }));
+// usePostEffect("final3", () => ({ "u_time": time(), "u_amount": 4, }));
