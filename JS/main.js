f8ec083ce5a69ea8477d69be84adbd7efeef9893
Joël Rimaz <jo.ante2011@gmail.com>
2023-12-15 11:27:00 +0100

    trying to get GitHub Pages to load...

--- JS/main.js
+++ JS/main.js
@@ -66,58 +66,55 @@ const level = addLevel([
 });
 
 /*------------------------------
-            SHADERS
+        SHADERS
 ------------------------------*/
 
 // loadShaderURL("KaboomJS_CRT", null, "../shaders/KaboomJS_shaders/CRT.frag");
 // loadShaderURL("KaboomJS_VHS", null, "../shaders/KaboomJS_shaders/VHS.frag");
-loadShaderURL("Default", null, "../shaders/base.frag");
-loadShaderURL("Siemens_screen", null, "../shaders/colourTest.frag");
-loadShaderURL("Black_and_white", null, "../shaders/blackAndWhiteTest.frag");
-loadShaderURL("Old_Macintosh_screen", null, "../shaders/macintoshScreen.frag");         // final #1
-loadShaderURL("Discoloration", null, "../shaders/discolourTest.frag");
-loadShaderURL("Old_VHS_on_CRT_screen", null, "../shaders/oldVHS.frag");                  // final #2
-loadShaderURL("Visual_noise", null, "../shaders/noiseTest.frag");
-loadShaderURL("Chroma_aberration", null, "../shaders/chromaAberration.frag");
-loadShaderURL("Chroma_aberration_wave", null, "../shaders/chromaAberrationAnim.frag");    // final #3
+loadShaderURL("Default", null, "/shaders/base.frag");
+loadShaderURL("Siemens_screen", null, "/shaders/colourTest.frag");
+loadShaderURL("Black_and_white", null, "/shaders/blackAndWhiteTest.frag");
+loadShaderURL("Old_Macintosh_screen", null, "/shaders/macintoshScreen.frag");         // final #1
+loadShaderURL("Discoloration", null, "/shaders/discolourTest.frag");
+loadShaderURL("Old_VHS_on_CRT_screen", null, "/shaders/oldVHS.frag");                  // final #2
+loadShaderURL("Visual_noise", null, "/shaders/noiseTest.frag");
+loadShaderURL("Chroma_aberration", null, "/shaders/chromaAberration.frag");
+loadShaderURL("Chroma_aberration_wave", null, "/shaders/chromaAberrationAnim.frag");    // final #3
 
+const effects = {
+    Default: () => ({}),
+    // KaboomJS_CRT: () => ({}),
+    Siemens_screen: () => ({}),
+    Black_and_white: () => ({}),
+    Old_Macintosh_screen: () => ({ "u_time": time(), "u_resy": height() }),
+    // KaboomJS_VHS: () => ({ "u_intensity": 10 }),
+    Discoloration:() => ({}),
+    Old_VHS_on_CRT_screen: () => ({ "u_time": time(), "u_blurIntensity": 0.5 }),
+    Visual_noise: () => ({ "u_time": time() }),
+    Chroma_aberration: () => ({ "u_amount": 2 }),
+    Chroma_aberration_wave: () => ({ "u_time": time(), "u_amount": 4 }),
+}
 
-setTimeout(() => {
-    const effects = {
-        Default: () => ({}),
-        // KaboomJS_CRT: () => ({}),
-        Siemens_screen: () => ({}),
-        Black_and_white: () => ({}),
-        Old_Macintosh_screen: () => ({ "u_time": time(), "u_resy": height() }),
-        // KaboomJS_VHS: () => ({ "u_intensity": 10 }),
-        Discoloration:() => ({}),
-        Old_VHS_on_CRT_screen: () => ({ "u_time": time(), "u_blurIntensity": 0.5 }),
-        Visual_noise: () => ({ "u_time": time() }),
-        Chroma_aberration: () => ({ "u_amount": 2 }),
-        Chroma_aberration_wave: () => ({ "u_time": time(), "u_amount": 4 }),
-    }
-    
-    let currentEffect = 0;
-    
-    onKeyPress("space", () => {
-        const list = Object.keys(effects);
-        currentEffect = (currentEffect + 1) % list.length;
-        label.text = list[currentEffect];
-    });
-    
-    onUpdate(() => {
-        const effect = Object.keys(effects)[currentEffect];
-        usePostEffect(effect, effects[effect]());
-    });
+let currentEffect = 0;
 
-    /*------------------------------
-            TEXT
-    ------------------------------*/
+onKeyPress("space", () => {
+    const list = Object.keys(effects);
+    currentEffect = (currentEffect + 1) % list.length;
+    label.text = list[currentEffect];
+});
 
-    const label = add([
-        pos(8, 8),
-        text(Object.keys(effects)[currentEffect], {
-            size: 10
-        }),
-    ]);
-}, 2000);
+onUpdate(() => {
+    const effect = Object.keys(effects)[currentEffect];
+    usePostEffect(effect, effects[effect]());
+});
+
+/*------------------------------
+        TEXT
+------------------------------*/
+
+const label = add([
+    pos(8, 8),
+    text(Object.keys(effects)[currentEffect], {
+        size: 10
+    }),
+]);
